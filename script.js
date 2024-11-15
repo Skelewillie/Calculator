81e9f89b1cd3f0539fb674105de8af2c3bb2a837
Fission <wilcentd@gmail.com>
2024-11-14 22:33:15 -0400

    Finish calculator with no extra credit

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
+function add(firstNumber,secondNumber){
+    return firstNumber+secondNumber;
+}
+
+function subtract(firstNumber, secondNumber){
+    return firstNumber-secondNumber;
+}
+
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber){
+    return firstNumber/secondNumber
+}
+
+function operate(operator, firstNumber, secondNumber){
+    let answer;
+
+    switch(operator){
+        case '+':
+            answer = add(firstNumber,secondNumber);
+            break
+        case '-':
+            answer = subtract(firstNumber,secondNumber);
+            break
+        case 'x':
+            answer = multiply(firstNumber,secondNumber);
+            break
+        case '/':
+            answer = divide(firstNumber,secondNumber).toFixed(2)
+    }
+
+    return answer
+}
+
+function evaluate(input,operators){
+    for(y in operators){
+        if (input.value.includes(operators[y])){
+
+            let operator = operators[y]
+            let firstNumber = parseInt(input.value.slice(0,input.value.indexOf(operator)))
+            let secondNumber = parseInt(input.value.slice(input.value.indexOf(operator)+1))
+
+            try{
+                input.value = operate(operator,firstNumber,secondNumber).toString()
+
+                if(isNaN(input.value)) throw 'Value is not a number. Ensure to enter the calculation correctly'
+                if(!(isFinite(input.value))) throw "You silly Billy. You can't divide by zero"
+            } catch (error) {
+                alert(error)
+            }
+            
+        }
+    }
+}
 const input = document.querySelector('#display');
-const clear = document.querySelector('#C');
 const keys = document.querySelector('.keys')
 
 let keyList = ['C','/','x','-','7','8','9','+','4','5','6','=','1','2','3','0']
 let operators = ['+','x','-','/']
+let operCount = 0
+
 for(x in keyList){
     const key = document.createElement('div')
     key.setAttribute('class', 'key');
@@ -12,23 +69,30 @@ for(x in keyList){
     
     key.addEventListener('click', () =>{
 
-        if(key.textContent == '='){
-            for(y in operators){
-                if (input.value.includes(operators[y])){
-                    let operator = operators[y]
-                    let firstNumber = input.value.slice(0,input.value.indexOf(operator))
-                    let lastNumber = input.value.slice(input.value.indexOf(operator)+1)
-                    console.log(firstNumber,operator,lastNumber)
-                }
+        if(operators.includes(key.textContent)){
+            operCount++
+
+            if(operCount = 2){
+                evaluate(input,operators)
+                
+                operCount = 0
             }
-        }else if(key.textContent != 'C'){
-            input.value += key.textContent
         }
 
+        if(key.textContent == '='){
+            evaluate(input,operators)
 
+            operCount = 0
 
+        }else if(key.textContent != 'C'){
+            input.value += key.textContent
+
+        }else{
+            input.value = ''
+        }
     })
 
 }
+
 
 
